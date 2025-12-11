63db497c173e37d600d2b5b4694d2c9d7f7f4a19
aloneen <seisenbek2005@mail.ru>
2025-12-11 16:54:36 +0500

    Created Comment & Category repos

---
+++ src/main/java/kz/seisen/blog/repositories/CommentRepository.java
@@ -0,0 +1,10 @@
+package kz.seisen.blog.repositories;
+
+
+import kz.seisen.blog.models.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
