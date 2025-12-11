9c54e8a64138000ec36ba62f3a51e0e13479b50f
aloneen <seisenbek2005@mail.ru>
2025-12-11 16:52:43 +0500

    Created User & Post repos

---
+++ src/main/java/kz/seisen/blog/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package kz.seisen.blog.repositories;
+
+
+import kz.seisen.blog.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
