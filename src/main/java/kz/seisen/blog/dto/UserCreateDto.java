be37eb08d473b24c993176e370e8af9d432adb95
aloneen <seisenbek2005@mail.ru>
2025-12-11 17:17:08 +0500

    Created Dto's for User ( creational and usual )

---
+++ src/main/java/kz/seisen/blog/dto/UserCreateDto.java
@@ -0,0 +1,14 @@
+package kz.seisen.blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCreateDto {
+    private String username;
+    private String email;
+    private String password;
+}
