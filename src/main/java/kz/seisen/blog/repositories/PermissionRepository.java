cd76ee6552e598e0bf5f0d6e3b28ff40e76f6fe7
aloneen <seisenbek2005@mail.ru>
2025-12-13 19:39:49 +0500

    Permission Repo

---
+++ src/main/java/kz/seisen/blog/repositories/PermissionRepository.java
@@ -0,0 +1,10 @@
+package kz.seisen.blog.repositories;
+
+import kz.seisen.blog.models.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+}
